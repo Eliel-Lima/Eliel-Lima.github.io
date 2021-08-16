1956c60c87a5191d49c9674699ede19685c3844a
Eliel Lima <eliel.contact@gmail.com>
2021-08-15 21:19:01 -0300

    Add files via upload

---
+++ js/cardQuestionEffect.js
@@ -0,0 +1,16 @@
+/*let cardHeader, cardContent,  cardIconShow;
+
+cardHeader = document.querySelectorAll('.c-cardQuestions__header')
+cardContent = document.querySelectorAll('.c-cardQuestions__content')
+cardIconShow = document.querySelectorAll('.cardIconShow')
+let count = 0
+
+function cardEffect(event) {
+   cardHeaderSelected = event.target.parentNode.parentNode
+   cardHeaderSelected.toggle('teste')
+   
+}
+
+cardIconShow.forEach((element,index) => {
+    element.addEventListener('click', cardEffect)
+})*/
